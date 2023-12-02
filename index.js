42030a77103a7c8de29db4bb2fc528fa1f8df1c7
Musab Al Mahi <musabalmahi53@gmail.com>
2023-12-02 17:09:40 +0600

    drive get operation into database.and get all the users into backendside to frontside

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ async function run() {
     const userCollection = client.db("medicoDb").collection("users");
 
     // user related api start here
+    app.get("/users",  async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
